f229663d2303189fca71f87f2221d329690fb238
Yibifur <103214248+Yibifur@users.noreply.github.com>
2024-02-09 11:01:58 +0300

    push

---
+++ MyUdemyProject/Frontend/HotelProject.WebUI/Controllers/AdminAboutController.cs
@@ -0,0 +1,56 @@
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult >Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5231/api/About");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var model = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(model);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5231/api/About/{id}");
+            if(responseMessage.IsSuccessStatusCode) { 
+            var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var model=JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(model);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto aboutModel)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(aboutModel);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage=await client.PutAsync("http://localhost:5231/api/About/", content);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
--- MyUdemyProject/Frontend/HotelProject.WebUI/obj/Release/net6.0/HotelProject.WebUI.AssemblyInfo.cs
+++ MyUdemyProject/Frontend/HotelProject.WebUI/obj/Release/net6.0/HotelProject.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelProject.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0d09d57066d3db4ccbe6b58b54ec7485b6df15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36250e6a7507c96446b0460f4343d50c64998559")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelProject.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelProject.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
